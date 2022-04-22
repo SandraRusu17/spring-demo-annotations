7626f698b6fc77a0ae46b900b51d5bc54e2f8bbe
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-04-22 11:54:06 +0300

    Define a new Coach implementation using Annotations

--- src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -9,7 +9,7 @@ public class AnnotationDemoApp {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         // get the bean from spring container
-        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+        Coach theCoach = context.getBean("danceCoach", Coach.class);
 
         // call a method on the bean
         System.out.println(theCoach.getDailyWorkout());
---
+++ src/com/luv2code/springdemo/DanceCoach.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DanceCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Dance for 2 hours";
+    }
+}
