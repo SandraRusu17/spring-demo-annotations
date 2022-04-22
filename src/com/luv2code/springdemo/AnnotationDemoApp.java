b792c2f85b25a35ec730dc62201db8e87135d74f
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-04-22 14:34:20 +0300

    Constructor Injection

--- src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -9,9 +9,10 @@ public class AnnotationDemoApp {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         // get the bean from spring container
-        Coach theCoach = context.getBean("danceCoach", Coach.class);
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
 
         // call a method on the bean
+        System.out.println(theCoach.getDailyFortune());
         System.out.println(theCoach.getDailyWorkout());
 
         // close the context
