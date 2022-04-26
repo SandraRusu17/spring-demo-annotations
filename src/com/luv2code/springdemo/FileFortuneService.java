58c035586ae059d0a89966e6c36aab875fd654ba
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-04-26 18:45:47 +0300

    Define a new implementation for FortuneService. Create a file with fortunes. Read from that file and return random fortunes. Inject the new dependency into Coach implementation.

---
+++ src/com/luv2code/springdemo/FileFortuneService.java
@@ -0,0 +1,51 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Component
+public class FileFortuneService implements FortuneService {
+
+    private String fileName = "C:\\Users\\srusu\\Personal Usage\\spring-demo-annotations\\src\\fortune-data.txt";
+    private List<String> theFortunes;
+
+    private Random random = new Random();
+
+    public FileFortuneService() {
+
+        File theFile = new File(fileName);
+
+        System.out.println("Reading fortunes from file ->" + theFile);
+        System.out.println("File exists ->" + theFile.exists());
+
+        // initialize the array list
+        theFortunes = new ArrayList<String>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String tempLine;
+
+            while ((tempLine = br.readLine()) != null) {
+                theFortunes.add(tempLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getFortune() {
+        int index = random.nextInt(theFortunes.size());
+
+        String tempFortune = theFortunes.get(index);
+
+        return tempFortune;
+    }
+}
--- src/com/luv2code/springdemo/SwimCoach.java
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -1,14 +1,37 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-public class SwimCoach implements Coach{
+@Component
+public class SwimCoach implements Coach {
+
+    @Autowired
+    private FileFortuneService fileFortuneService;
+
 
     @Value("${foo.email}")
     private String email;
 
     @Value("${foo.team}")
     private String team;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
 
     @Override
     public String getDailyWorkout() {
@@ -17,6 +40,6 @@ public class SwimCoach implements Coach{
 
     @Override
     public String getDailyFortune() {
-        return null;
+        return fileFortuneService.getFortune();
     }
 }
