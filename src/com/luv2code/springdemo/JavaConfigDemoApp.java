d0afefb70344ca832424f8665d2902a7a0fc3fdc
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-05-05 15:03:49 +0300

    Injecting Values from Properties File

--- src/com/luv2code/springdemo/JavaConfigDemoApp.java
+++ src/com/luv2code/springdemo/JavaConfigDemoApp.java
@@ -11,11 +11,15 @@ public class JavaConfigDemoApp {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 
         // get the bean from spring container
-        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+        SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 
         // call a method on the bean
         System.out.println(theCoach.getDailyFortune());
         System.out.println(theCoach.getDailyWorkout());
+
+        // call our new swim coach methods...with prop values injected
+        System.out.println("email" + theCoach.getEmail());
+        System.out.println("team" + theCoach.getTeam());
 
         // close the context
         context.close();
--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -2,10 +2,12 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
-@ComponentScan("com.luv2code.springdemo")
+//@ComponentScan("com.luv2code.springdemo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 
     // define bean for our fortune service
